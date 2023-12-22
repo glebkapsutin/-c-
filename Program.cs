a1d630ac5a2358a5d816dcba53e573515d57c153
gleb <gleb.kapustin@outlook.com>
2023-12-22 13:50:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Net;
+
+namespace Изучение_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = [2, 5, 13, 7, 6, 4];
+            int size = numbers.Length;
+            int summ = 0;
+            int avg = 0;
+            int index = 0;
+            while (index < size)
+            {
+                summ += numbers[index];
+
+                index++;
+
+            }
+            Console.WriteLine(avg = summ / size);
+
+
+        }
+    }
+}
